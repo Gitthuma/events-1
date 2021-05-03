09668e1418415296665f13dced01dac017ffb82b
Gitthuma <george.githuma@gmail.com>
2021-05-04 00:05:35 +0300

    Created a simple event handler that causes the text inside the button (btn) to change when it is clicked on, and change back when it is clicked again.

--- code/main.js
+++ code/main.js
@@ -3,3 +3,12 @@ let btn = document.querySelector('.off');
 // Add your code here
 
 /*Create a simple event handler that causes the text inside the button (btn) to change when it is clicked on, and change back when it is clicked again.*/
+
+btn.onclick = function(e) {
+    if (btn.textContent === 'Machine is off') {
+        btn.textContent = 'Machine is on';
+    } else {
+        btn.textContent = 'Machine is off'
+    }
+    
+}
