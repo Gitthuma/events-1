8c6a192080b676ce6f5a39e908ae5872ee37e9bc
Gitthuma <george.githuma@gmail.com>
2021-05-03 23:58:30 +0300

    Created a simple event handler that causes the text inside the button (btn) to change when it is clicked on, and change back when it is clicked again.

--- code/main.js
+++ code/main.js
@@ -1,3 +1,5 @@
 let btn = document.querySelector('.off');
 
-    // Add your code here
+// Add your code here
+
+/*Create a simple event handler that causes the text inside the button (btn) to change when it is clicked on, and change back when it is clicked again.*/
